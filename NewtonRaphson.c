22f887495cab417a7fdb8741b4d7bdd3419d8941
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-12-10 20:18:40 +0530

    Update NewtonRaphson.c

--- NewtonRaphson.c
+++ NewtonRaphson.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
-#define e 0.001
+#define e 0.0001 // Adjust the precision by changing the value of 'e'
+
 double funct(double x)
 {
     return x * x * x - x - 1.0;
 }
+
 double functDash(double x)
 {
     return 3.0 * x * x - 1.0;
 }
+
 void newtonRaphson(double x)
 {
     double h = funct(x) / functDash(x);
+
     while (fabs(h) >= e)
     {
         h = funct(x) / functDash(x);
         x = x - h;
-        printf("INtere\n");
     }
-    printf("%f ", x);
+
+    printf("Root: %f\n", x);
 }
+
 int main()
 {
     newtonRaphson(1.5);
